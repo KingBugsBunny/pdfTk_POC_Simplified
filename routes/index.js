b93553d330551cdbcb8722f547859261c0a2fdc0
RobertCawBrush <robert.brush@brieBug.com>
2016-02-05 16:17:05 -0700

    added readme and commented code

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+  //file paths need to follow a 'variable initialisation' in order to be called correctly
   var pdf = 'A=' + '/PDPmergeDocs/New-app-rpt-cover-with-fields.pdf' +
       ' B=' + '/PDPmergeDocs/3.0ReportLayout.pdf' +
        ' C=/' + 'PDPmergeDocs/New-app-rpt-covers-ProJobTeamSpecs.pdf';
 
+  //to call a range call the paths variable name and concat the range on the end such as A1-end
+  //which will print the first page to the end of the document
   pdfTk.pages(pdf,'A1-end B1-end', 'Reports1', function(err) {
     var message = '';
     if(err){
@@ -19,6 +22,7 @@ router.get('/', function(req, res, next) {
     return console.log(message);
   });
 
+  //to print a page by itself just add one number without a range such as C2
   pdfTk.pages(pdf,'C2 B1-2', 'Reports2', function(err) {
     var message = '';
     if(err){
